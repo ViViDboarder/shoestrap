765afa924ea17a70c93dd42888bed5bfacf414e6
ViViDboarder <ViViDboarder@gmail.com>
2015-11-07 11:30:58 -0800

    Add the ability to source local versions of any additional omf config file

--- assets/default/fish-config/init.fish
+++ assets/default/fish-config/init.fish
@@ -1,2 +1,14 @@
-source $OMF_CONFIG/paths.fish
-source $OMF_CONFIG/alias.fish
+function source_config
+    # Sources a config file and corresponding local config file if it exists
+    set shared_config "$OMF_CONFIG/$argv[1].fish"
+    set local_config "$OMF_CONFIG/$argv[1].local.fish"
+    if test -f "$shared_config"
+        source "$shared_config"
+    end
+    if test -f "$local_config"
+        source "$local_config"
+    end
+end
+
+source_config paths
+source_config alias
