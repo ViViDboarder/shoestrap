96e35a7ebe1ea294c780be43bbb5927f9cb28fb1
ViViDboarder <ViViDboarder@gmail.com>
2016-02-04 10:33:08 -0800

    Small fixups of paths

--- assets/default/fish-config/paths.fish
+++ assets/default/fish-config/paths.fish
@@ -10,24 +10,28 @@ switch (uname)
 end
 
 # opt directory
-set -gx PATH /opt/local/bin /opt/local/sbin $PATH
+if [ -d /opt/local ]
+    set -gx PATH /opt/local/bin /opt/local/sbin $PATH
+end
+
+# Google GO
+set -gx GOROOT /usr/local/go
+if [ -d "$GOROOT" ]
+    set -gx PATH $PATH $GOROOT/bin
+end
 
 if [ $det_os = "linux" ]
     set android_sdk $HOME/workspace/adt-bundle-linux/sdk
-
-    set -gx GOROOT /usr/local/go
-    set -gx PATH $PATH $GOROOT/bin
 else if [ $det_os = "mac" ]
     set android_sdk $HOME/workspace/android-sdk-macosx
+
+    set go_workspace $HOME/workspace/go_workspace
+    if [ -d "$go_workspace" ]
+        set -gx GOPATH $go_workspace
+        set -gx PATH $PATH $GOPATH/bin
+    end
+
     # set PATH $HOME/Library/Python/2.7/bin $PATH
-
-    set -gx GOROOT /usr/local/go
-    set -gx PATH $PATH $GOROOT/bin
-
-    set go_workspace $HOME/workspace/go
-    set -gx PATH $PATH $go_workspace/bin
-    set -gx GOPATH $go_workspace $GOROOT
-
     # Fix Python path on OSX to avoid considering System extras over newer versions
     set -gx PYTHONPATH /opt/local/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/site-packages /Library/Python/2.7/site-packages $PYTHONPATH
 end
@@ -38,9 +42,11 @@ end
 # set -gx PATH $bin_path $PATH
 # set -gx PYTHONPATH "$sdk_dir/platform/google_appengine" $PYTHONPATH
 
+# Android paths
 set -gx ANDROID_HOME $android_sdk
 set -gx PATH $PATH $android_sdk/platform-tools $android_sdk/tools
 
+# Home path
 set -gx  PATH $HOME/bin $PATH
 
 #[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
