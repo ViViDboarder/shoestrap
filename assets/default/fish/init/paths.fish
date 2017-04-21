0250897a4b6134375319728a87340e84dee01034
ViViDboarder <ViViDboarder@gmail.com>
2017-04-21 13:20:23 -0700

    Set a default VIM_COLOR and prevent overriding if provided

--- assets/default/fish/init/paths.fish
+++ assets/default/fish/init/paths.fish
@@ -55,3 +55,6 @@ set -gx ANT_OPTS "-Xmx2048m -Xms512m"
 # FZF
 set -gx FZF_DEFAULT_COMMAND 'ag -g ""'
 set -gx FZF_CTRL_T_COMMAND "$FZF_DEFAULT_COMMAND \$dir"
+
+# Vim Colors so that they can be set by env
+set -q VIM_COLOR; or set -gx VIM_COLOR wombat256mod
