cd1155792bf11c41f2ba80098ae90a297032da3f
ViViDboarder <ViViDboarder@gmail.com>
2017-11-16 10:22:02 -0800

    Fix paths for rvm and pip

--- assets/default/fish/init/paths.fish
+++ assets/default/fish/init/paths.fish
@@ -25,6 +25,7 @@ else if [ $det_os = "mac" ]
     # set PATH $HOME/Library/Python/2.7/bin $PATH
     # Fix Python path on OSX to avoid considering System extras over newer versions
     set -gx PYTHONPATH /opt/local/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/site-packages:$PYTHONPATH
+    set -gx PATH $PATH /opt/local/Library/Frameworks/Python.framework/Versions/Current/bin
 end
 
 # Google GO
@@ -36,6 +37,12 @@ end
 if [ -d "$android_sdk" ]
     set -gx ANDROID_HOME $android_sdk
     set -gx PATH $PATH $android_sdk/platform-tools $android_sdk/tools
+end
+
+# Ruby paths
+# Add RVM to PATH for scripting
+if [ -d "$HOME/.rvm" ]
+    set -gx PATH $PATH $HOME/.rvm/bin
 end
 
 # Home path
