0297379eacc92c7fdb8c284138629106c1e16c25
ViViDboarder <ViViDboarder@gmail.com>
2019-04-30 15:46:43 -0700

    fish: Add XDG env variables

--- assets/default/fish/init/env.fish
+++ assets/default/fish/init/env.fish
@@ -1,3 +1,8 @@
+# XDG Spec
+set -gx XDG_CONFIG_HOME "$HOME/.config"
+set -gx XDG_DATA_HOME "$HOME/.local/share"
+set -gx XDG_CACHE_HOME "$HOME/.cache"
+
 # Increase memory sizes for java using Ant
 set -gx ANT_OPTS "-Xmx2048m -Xms512m"
 
