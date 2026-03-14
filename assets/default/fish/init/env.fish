3b29f6342a7d281db4e444af97f3420e98baf382
ViViDboarder <vividboarder@gmail.com>
2026-03-13 17:08:56 -0700

    Fix fish theme switching

--- assets/default/fish/init/env.fish
+++ assets/default/fish/init/env.fish
@@ -44,8 +44,11 @@ if status --is-interactive
     # Set fish theme based on newly exported colors
     if set -q FISH_THEME
         # Use builtin theme management if available
-        fish_config theme list | grep -q "$FISH_THEME"; and fish_config theme choose "$FISH_THEME"
-        # Otherwise check custom
-        fish_theme | grep -q "$FISH_THEME"; and fish_theme "$FISH_THEME"
+        if fish_config theme list | grep -q "$FISH_THEME"
+            fish_config theme choose "$FISH_THEME"
+        else
+            # Otherwise set custom
+            fish_theme "$FISH_THEME"
+        end
     end
 end
